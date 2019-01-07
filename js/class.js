eea80e56f1bc3c0e52aa671420c4a3838505a213
mr.scrpt <dolcegabbana2010@gmail.com>
2019-01-07 12:50:35 +0200

    work project

--- js/class.js
+++ js/class.js
@@ -1,152 +1,180 @@
 class MyVk {
-    constructor(v, gen) {
-        this.paramsSelf = {v: v || '5.92', name_case: gen || 'gen'};
-        this.paramsFriends = {v: v || '5.92', fields: "photo_100"};
-    }
-    login(appId, perms) { //6784868 //2
-        return new Promise((resolve, reject) => {
-            VK.init({
-                apiId: appId //6784868
-            });
-            VK.Auth.login(data => {
-                if (data.session) {
-                    resolve(data);
-                } else {
-                    reject("Ошибка авторизации");
-                }
-            }, perms); //2
-        })
-    }
-    callApi(method, params) {
-        return new Promise((resolve, reject) => {
-            VK.api(method, params, (data) => {
-                if (data.error) {
-                    reject(data.error);
-                } else {
-                    resolve(data.response);
-                }
-            })
-        })
-    }
-    getSelf() {
-        return this.callApi('users.get', this.paramsSelf);
+	constructor(v, gen) {
+		this.paramsSelf = {v: v || '5.92', name_case: gen || 'gen'};
+		this.paramsFriends = {v: v || '5.92', fields: 'photo_100'};
+	}
 
-    }
-    getSelfItem(data) {
-        let [me] = data;
-        return me;
-    }
-    getFriends() {
-        return this.callApi('friends.get', this.paramsFriends);
-    }
-    getFriendsList(data) {
-        let arr = [];
-        for (let row in data.items) {
-            arr.push(data.items[row]);
-        }
-        return arr;
-    }
-    filterList(arr, params) {
-        let res = [];
-        let regexp = new RegExp(params, "i");
-        arr.forEach((item) => {
-            if (item.first_name.search(regexp) > -1 || item.last_name.search(regexp) > -1) {
-                //console.log(item);
-                res.push(item);
-                return true;
-            } else {
-                return false;
-            }
+	login(appId, perms) { //6784868 //2
+		return new Promise((resolve, reject) => {
+			VK.init({
+				apiId: appId //6784868
+			});
+			VK.Auth.login(data => {
+				if (data.session) {
+					resolve(data);
+				} else {
+					reject('Ошибка авторизации');
+				}
+			}, perms); //2
+		});
+	}
 
-        });
-        return res;
+	callApi(method, params) {
+		return new Promise((resolve, reject) => {
+			VK.api(method, params, (data) => {
+				if (data.error) {
+					reject(data.error);
+				} else {
+					resolve(data.response);
+				}
+			});
+		});
+	}
 
-    }
-    save(storage, name, arr) {
-        let data = JSON.stringify(arr);
-        if (storage === 'local') {
-            localStorage[name] = data;
-        }
-        if (storage === 'session') {
-            sessionStorage[name] = data;
-        }
+	getSelf() {
+		return this.callApi('users.get', this.paramsSelf);
 
-    }
-    load(storage, name) {
-        let arr = [];
-        if (storage === 'local') {
-            arr = JSON.parse(localStorage[name]);
-        }
-        if (storage === 'session') {
-            arr = JSON.parse(sessionStorage[name]);
-        }
-        return arr;
-    }
-    loadListName(storage) {
-        let s = "";
-        let name = [];
-        if (storage === "local") {
-            s = localStorage;
-        } else if (storage === "session") {
-            s = sessionStorage;
-        } else {
-            throw new Error('Харнилище не найдено');
-        }
-        for (let i = 0; i < s.length; i++) {
-            let key = s.key(i);
-            name.push(key);
-        }
-        if (name.length === 0) {
-            return;
-        }
-        return name;
-    }
-    loadList(storage, name, url) {
-        let arr = [];
-        if (name === url) {
-            return;
-        } else if (localStorage[name] || sessionStorage[name]) {
-            if (storage === "local") {
-                arr = JSON.parse(localStorage[name]);
-            } else if (storage === "session") {
-                arr = JSON.parse(sessionStorage[name]);
-            } else if (!arr.length) {
-                throw new Error('список пустой');
-            }
-            return arr;
-        } else {
-            throw new Error('список не существует')
-        }
+	}
 
-    }
-    renderList(storage) {
-        //Выводим сохраненные списки при клике
-        let list = this.loadListName(storage);
-        if (list) {
+	getSelfItem(data) {
+		let [me] = data;
+		return me;
+	}
 
-            View.renderList(list, friendFave)
-        }
-        ;
-    }
-    saveURL(name) {
-        history.pushState({}, 'new state', '/list=' + name);
+	getFriends() {
+		return this.callApi('friends.get', this.paramsFriends);
+	}
 
-    }
-    loadURL(name) {
-        try {
-            history.pushState(null, null, '/list=' + name);
-            return;
-        } catch (e) {
-            console.error(e)
-        }
-        location.hash = '/list=' + name;
-    }
-    getURL(separator) {
-        let url = decodeURIComponent(location.pathname);
-        let from = url.search(separator) + 1;
-        let to = url.length;
-        return url.substring(from, to)
-    }
+	getFriendsList(data) {
+		let arr = [];
+		for (let row in data.items) {
+			arr.push(data.items[row]);
+		}
+		return arr;
+	}
+
+	filterList(arr, params) {
+		let res = [];
+		let regexp = new RegExp(params, 'i');
+		arr.forEach((item) => {
+			if (item.first_name.search(regexp) > -1 || item.last_name.search(regexp) > -1) {
+				//console.log(item);
+				res.push(item);
+				return true;
+			} else {
+				return false;
+			}
+
+		});
+		return res;
+
+	}
+
+	save(storage, name, arr) {
+		let data = JSON.stringify(arr);
+		if (storage === 'local') {
+			localStorage[name] = data;
+		}
+		if (storage === 'session') {
+			sessionStorage[name] = data;
+		}
+
+	}
+
+	load(storage, name) {
+		let arr = [];
+		if (storage === 'local') {
+			arr = JSON.parse(localStorage[name]);
+		}
+		if (storage === 'session') {
+			arr = JSON.parse(sessionStorage[name]);
+		}
+		return arr;
+	}
+
+	loadListName(storage) {
+		let s = '';
+		let name = [];
+		if (storage === 'local') {
+			s = localStorage;
+		} else if (storage === 'session') {
+			s = sessionStorage;
+		} else {
+			throw new Error('Харнилище не найдено');
+		}
+		for (let i = 0; i < s.length; i++) {
+			let key = s.key(i);
+			name.push(key);
+		}
+		if (name.length === 0) {
+			return;
+		}
+		return name;
+	}
+
+	loadList(storage, name) {
+		let arr = [];
+		if (name === '/') {
+			return;
+		} else if (localStorage[name] || sessionStorage[name]) {
+			if (storage === 'local') {
+				arr = JSON.parse(localStorage[name]);
+			} else if (storage === 'session') {
+				arr = JSON.parse(sessionStorage[name]);
+			} else if (!arr.length) {
+				throw new Error('список пустой');
+			}
+			return arr;
+		} else {
+			throw new Error('список не существует');
+		}
+
+	}
+
+	renderList(storage) {
+		//Выводим сохраненные списки при клике
+		let list = this.loadListName(storage);
+		if (list) {
+
+			View.renderList(list, friendFave);
+		}
+		;
+	}
+
+	saveURL(name) {
+		history.pushState({}, 'new state', '/list=' + name);
+
+	}
+
+	loadURL(name) {
+		try {
+			history.pushState(null, null, '/list=' + name);
+			return;
+		} catch (e) {
+			console.error(e);
+		}
+		location.hash = '/list=' + name;
+	}
+
+	getURL(list, separator) {
+		let url = decodeURIComponent(location.pathname);
+		let regexp = new RegExp(list, 'i');
+
+		if (url.search(regexp) === -1) {
+			return false;
+		}
+		let from = url.search(separator) + 1;
+		let to = url.length;
+		return url.substring(from, to);
+	}
+
+	destroy(elem) {
+		let list = elem.querySelectorAll('*');
+		for (let i = 0; i < list.length; i++) {
+			list[i].remove();
+			console.log(list[i]);
+		}
+	}
 }
 
-export {MyVk}
+export {MyVk};
--- js/controller.js
+++ js/controller.js
@@ -15,64 +15,70 @@ window.addEventListener('DOMContentLoaded', () => {
 
     (async ()=>{
         await worker.login(6808614, 2);
-        const self = await worker.getSelf();
-        let me = worker.getSelfItem(self);
-        view.renderMe(me, title);
-        //Получаем всех друзей и загружаем их
-        const friends = await worker.getFriends();
-        let friendsData = await worker.getFriendsList(friends);
-        view.renderFriends(friendsData, friendList);
+		const self = await worker.getSelf();
+		let me = worker.getSelfItem(self);
+		view.renderMe(me, title);
+		//Получаем всех друзей и загружаем их
+		const friends = await worker.getFriends();
+		let friendsData = await worker.getFriendsList(friends);
+		view.renderFriends(friendsData, friendList);
 
-        //Загрузка списка из УРЛ
-        let activeListName = worker.getURL('=');
-        if(activeListName){
-          let activeList = worker.loadList('local', activeListName, '/fl/index.html');
-          view.renderFriends(activeList, friendFilter);
-        }
+		//Загрузка списка из УРЛ
+		let activeListName = worker.getURL('list','=');
+		console.log(activeListName);
+		if(activeListName !== false){
+			let activeList = worker.loadList('local', activeListName);
+			view.renderFriends(activeList, friendFilter);
+		}
 
 
 
 
-        //Фильтруем друзей
-        inputFilter.addEventListener('keyup', async (e)=>{
-            let value = e.target.value;
-            let filter = await worker.filterList(friendsData, value);
-            view.renderFriends(filter, friendFilter);
-            worker.save('session', 'temp', filter);
-        });
-        //Запоминаем списки
-        add.addEventListener('click', async ()=>{
-            let value = inputNameList.value;
-            let temp = worker.load('session', 'temp');
-            worker.save('local', value, temp);
-            let list = worker.loadListName('local');
-            view.renderList(list, friendFave);
+		//Фильтруем друзей
+		inputFilter.addEventListener('keyup', async (e)=>{
+			let value = e.target.value;
+			let filter = await worker.filterList(friendsData, value);
+			view.renderFriends(filter, friendFilter);
+			worker.save('session', 'temp', filter);
+		});
+		//Запоминаем списки
+		add.addEventListener('click', async ()=>{
+			let value = inputNameList.value;
+			let temp = worker.load('session', 'temp');
+			worker.save('local', value, temp);
+			let list = worker.loadListName('local');
+			view.renderList(list, friendFave);
 
-            worker.saveURL(value);
-        });
-        //Выводим сохраненные списки
-        let list = worker.loadListName('local');
-        view.renderList(list, friendFave);
+			worker.saveURL(value);
+		});
+		//Выводим сохраненные списки
+		let list = worker.loadListName('local');
+		view.renderList(list, friendFave);
 
-        //Загружаем список по клику
-        friendFave.addEventListener('click', async (e)=>{
-            let item = e.target;
-            if(item.tagName === "A"){
-                e.preventDefault();
-                let value = item.innerText;
-                let res = worker.loadList('local', value);
-                view.renderFriends(res, friendFilter);
-                worker.loadURL(value);
-            }
-        });
+		//Загружаем список по клику
+		friendFave.addEventListener('click', async (e)=>{
+			let item = e.target;
+			if(item.tagName === "A"){
+				e.preventDefault();
+				let value = item.innerText;
+				let res = worker.loadList('local', value);
+				view.renderFriends(res, friendFilter);
+				worker.loadURL(value);
+			}
+		});
 
-        window.addEventListener('popstate', () => {
-            let activeListName = worker.getURL('=');
-            let activeList = worker.loadList('local', activeListName);
-            view.renderFriends(activeList, friendFilter);
-        })
+		window.addEventListener('popstate', () => {
+			let activeListName = worker.getURL('list','=');
+			if(activeListName !== false){
+				let activeList = worker.loadList('local', activeListName);
+				view.renderFriends(activeList, friendFilter);
+			}else {
+				worker.destroy(friendFilter );
+			}
 
-    })();
+		})
+
+	})();
 
 });
 
